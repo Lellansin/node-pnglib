8c3ec17dae87cd9d16363fce6f275e96f604344c
Lellansin <lellansin@gmail.com>
2017-07-24 20:57:46 +0800

    feat: upgrade String Array to inner Buffer

--- pnglib.js
+++ pnglib.js
@@ -1,5 +1,40 @@
 'use strict';
 
+const ENCODING = 'ascii';
+const CODE_NUL = Buffer.from('\x00', ENCODING);
+const CODE_SOH = Buffer.from('\x01', ENCODING);
+const CODE_X08X03 = Buffer.from('\x08\x03', ENCODING);
+const PNG_IHDR = Buffer.from('IHDR', ENCODING);
+const PNG_PLTE = Buffer.from('PLTE', ENCODING);
+const PNG_tRNS = Buffer.from('tRNS', ENCODING);
+const PNG_IDAT = Buffer.from('IDAT', ENCODING);
+const PNG_IEND = Buffer.from('IEND', ENCODING);
+const PNG_HEAD = Buffer.from('\x89PNG\r\n\x1a\n', ENCODING);
+
+// deflate header
+const PNG_DEFLATE_HEADER = function() {
+  let header = ((8 + (7 << 4)) << 8) | (3 << 6);
+  header += 31 - (header % 31);
+  return header;
+}();
+
+// Create crc32 lookup table
+const CRC_TABLE = function () {
+  let _crc32 = new Array();
+  for (let i = 0; i < 256; i++) {
+    let c = i;
+    for (let j = 0; j < 8; j++) {
+      if (c & 1) {
+        c = -306674912 ^ ((c >> 1) & 0x7fffffff);
+      } else {
+        c = (c >> 1) & 0x7fffffff;
+      }
+    }
+    _crc32[i] = c;
+  }
+  return _crc32;
+}();
+
 class PNGlib {
   constructor (width, height, depth) {
     this.width   = width;
@@ -13,7 +48,7 @@ class PNGlib {
     this.data_size = 2 + this.pix_size + 5 * Math.floor((0xfffe + this.pix_size) / 0xffff) + 4;
 
     // offsets and sizes of Png chunks
-    this.ihdr_offs = 0;         // IHDR offset and size
+    this.ihdr_offs = 0;                               // IHDR offset and size
     this.ihdr_size = 4 + 4 + 13 + 4;
     this.plte_offs = this.ihdr_offs + this.ihdr_size; // PLTE offset and size
     this.plte_size = 4 + 4 + 3 * depth + 4;
@@ -25,123 +60,81 @@ class PNGlib {
     this.iend_size = 4 + 4 + 4;
     this.buffer_size  = this.iend_offs + this.iend_size;  // total PNG size
 
-    this.buffer  = new Array();
-    this.palette = new Object();
+    this.buffer  = Buffer.alloc(this.buffer_size);
+    this.palette = {};
     this.pindex  = 0;
 
-    this._crc32 = new Array();
-
-    // initialize buffer with zero bytes
-    for (var i = 0; i < this.buffer_size; i++) {
-      this.buffer[i] = '\x00';
-    }
-
     // initialize non-zero elements
-    write(this.buffer, this.ihdr_offs, byte4(this.ihdr_size - 12), 'IHDR', byte4(width), byte4(height), '\x08\x03');
-    write(this.buffer, this.plte_offs, byte4(this.plte_size - 12), 'PLTE');
-    write(this.buffer, this.trns_offs, byte4(this.trns_size - 12), 'tRNS');
-    write(this.buffer, this.idat_offs, byte4(this.idat_size - 12), 'IDAT');
-    write(this.buffer, this.iend_offs, byte4(this.iend_size - 12), 'IEND');
+    write(this.buffer, this.ihdr_offs, byte4(this.ihdr_size - 12), PNG_IHDR, byte4(width), byte4(height), CODE_X08X03);
+    write(this.buffer, this.plte_offs, byte4(this.plte_size - 12), PNG_PLTE);
+    write(this.buffer, this.trns_offs, byte4(this.trns_size - 12), PNG_tRNS);
+    write(this.buffer, this.idat_offs, byte4(this.idat_size - 12), PNG_IDAT);
+    write(this.buffer, this.iend_offs, byte4(this.iend_size - 12), PNG_IEND);
 
     // initialize deflate header
-    var header = ((8 + (7 << 4)) << 8) | (3 << 6);
-    header+= 31 - (header % 31);
-
-    write(this.buffer, this.idat_offs + 8, byte2(header));
+    write(this.buffer, this.idat_offs + 8, byte2(PNG_DEFLATE_HEADER));
 
     // initialize deflate block headers
-    for (var i = 0; (i << 16) - 1 < this.pix_size; i++) {
-      var size, bits;
+    for (let i = 0; (i << 16) - 1 < this.pix_size; i++) {
+      let size, bits;
       if (i + 0xffff < this.pix_size) {
         size = 0xffff;
-        bits = '\x00';
+        bits = CODE_NUL;
       } else {
         size = this.pix_size - (i << 16) - i;
-        bits = '\x01';
+        bits = CODE_SOH;
       }
       write(this.buffer, this.idat_offs + 8 + 2 + (i << 16) + (i << 2), bits, byte2lsb(size), byte2lsb(~size));
-    }
-
-    /* Create crc32 lookup table */
-    for (var i = 0; i < 256; i++) {
-      var c = i;
-      for (var j = 0; j < 8; j++) {
-        if (c & 1) {
-          c = -306674912 ^ ((c >> 1) & 0x7fffffff);
-        } else {
-          c = (c >> 1) & 0x7fffffff;
-        }
-      }
-      this._crc32[i] = c;
     }
   }
 
   // compute the index into a png for a given pixel
-  index(x,y) {
-    var i = y * (this.width + 1) + x + 1;
-    var j = this.idat_offs + 8 + 2 + 5 * Math.floor((i / 0xffff) + 1) + i;
+  index(x, y) {
+    let i = y * (this.width + 1) + x + 1;
+    let j = this.idat_offs + 8 + 2 + 5 * Math.floor((i / 0xffff) + 1) + i;
     return j;
   }
 
   // convert a color and build up the palette
   color(red, green, blue, alpha) {
-
     alpha = alpha >= 0 ? alpha : 255;
-    var color = (((((alpha << 8) | red) << 8) | green) << 8) | blue;
+    const color = (((((alpha << 8) | red) << 8) | green) << 8) | blue;
 
     if (typeof this.palette[color] == 'undefined') {
-      if (this.pindex == this.depth) return '\x00';
+      if (this.pindex == this.depth) return CODE_NUL;
 
-      var ndx = this.plte_offs + 8 + 3 * this.pindex;
+      let ndx = this.plte_offs + 8 + 3 * this.pindex;
 
-      this.buffer[ndx + 0] = String.fromCharCode(red);
-      this.buffer[ndx + 1] = String.fromCharCode(green);
-      this.buffer[ndx + 2] = String.fromCharCode(blue);
-      this.buffer[this.trns_offs+8+this.pindex] = String.fromCharCode(alpha);
+      this.buffer[ndx + 0] = red;
+      this.buffer[ndx + 1] = green;
+      this.buffer[ndx + 2] = blue;
+      this.buffer[this.trns_offs + 8 + this.pindex] = alpha;
 
-      this.palette[color] = String.fromCharCode(this.pindex++);
+      this.palette[color] = this.pindex++;
     }
     return this.palette[color];
   }
 
   // output a PNG string, Base64 encoded
   getBase64() {
-    var s = this.getDump();
-
-    var ch = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-    var c1, c2, c3, e1, e2, e3, e4;
-    var l = s.length;
-    var i = 0;
-    var r = '';
-
-    do {
-      c1 = s.charCodeAt(i);
-      e1 = c1 >> 2;
-      c2 = s.charCodeAt(i+1);
-      e2 = ((c1 & 3) << 4) | (c2 >> 4);
-      c3 = s.charCodeAt(i+2);
-      if (l < i+2) { e3 = 64; } else { e3 = ((c2 & 0xf) << 2) | (c3 >> 6); }
-      if (l < i+3) { e4 = 64; } else { e4 = c3 & 0x3f; }
-      r+= ch.charAt(e1) + ch.charAt(e2) + ch.charAt(e3) + ch.charAt(e4);
-    } while ((i+= 3) < l);
-    return r;
+    return Buffer.concat([PNG_HEAD, this.getDump()]).toString('base64');
   }
 
-  // output a PNG string
+  // get PNG buffer
   getDump() {
-
     // compute adler32 of output pixels + row filter bytes
-    var BASE = 65521; /* largest prime smaller than 65536 */
-    var NMAX = 5552;  /* NMAX is the largest n such that 255n(n+1)/2 + (n+1)(BASE-1) <= 2^32-1 */
-    var s1 = 1;
-    var s2 = 0;
-    var n = NMAX;
+    const BASE = 65521; /* largest prime smaller than 65536 */
+    const NMAX = 5552;  /* NMAX is the largest n such that 255n(n+1)/2 + (n+1)(BASE-1) <= 2^32-1 */
 
-    for (var y = 0; y < this.height; y++) {
-      for (var x = -1; x < this.width; x++) {
-        s1 += this.buffer[this.index(x, y)].charCodeAt(0);
+    let s1 = 1;
+    let s2 = 0;
+    let n = NMAX;
+
+    for (let y = 0; y < this.height; y++) {
+      for (let x = -1; x < this.width; x++) {
+        s1 += this.buffer[this.index(x, y)];
         s2 += s1;
-        if ((n-= 1) == 0) {
+        if ((n -= 1) == 0) {
           s1 %= BASE;
           s2 %= BASE;
           n = NMAX;
@@ -153,44 +146,39 @@ class PNGlib {
     write(this.buffer, this.idat_offs + this.idat_size - 8, byte4((s2 << 16) | s1));
 
     // compute crc32 of the PNG chunks
-    let crc32 = (png, offs, size) => {
-      var crc = -1;
-      for (var i = 4; i < size-4; i += 1) {
-        crc = this._crc32[(crc ^ png[offs+i].charCodeAt(0)) & 0xff] ^ ((crc >> 8) & 0x00ffffff);
-      }
-      write(png, offs + size-4, byte4(crc ^ -1));
-    }
-
     crc32(this.buffer, this.ihdr_offs, this.ihdr_size);
     crc32(this.buffer, this.plte_offs, this.plte_size);
     crc32(this.buffer, this.trns_offs, this.trns_size);
     crc32(this.buffer, this.idat_offs, this.idat_size);
     crc32(this.buffer, this.iend_offs, this.iend_size);
 
-    // convert PNG to string
-    return '\x89PNG\r\n\x1a\n' + this.buffer.join('');
+    return this.buffer;
   }
 }
 
 // helper functions for that ctx
-function write(buffer, offs, ...args) {
-  args.map((param) => {
-    for (var i = 0; i < param.length; i++) {
-      buffer[offs++] = param.charAt(i);
-    }
-  });
+function write(buf, offs, ...args) {
+  Buffer.concat(args).copy(buf, offs);
 }
 
 function byte2(w) {
-  return String.fromCharCode((w >> 8) & 255, w & 255);
+  return Buffer.from([(w >> 8) & 255, w & 255]);
 }
 
 function byte4(w) {
-  return String.fromCharCode((w >> 24) & 255, (w >> 16) & 255, (w >> 8) & 255, w & 255);
+  return Buffer.from([(w >> 24) & 255, (w >> 16) & 255, (w >> 8) & 255, w & 255]);
 }
 
 function byte2lsb(w) {
-  return String.fromCharCode(w & 255, (w >> 8) & 255);
+  return Buffer.from([w & 255, (w >> 8) & 255]);
+}
+
+function crc32(buf2, offs, size) {
+  let crc = -1;
+  for (let i = 4; i < size - 4; i += 1) {
+    crc = CRC_TABLE[(crc ^ buf2[offs+i]) & 0xff] ^ ((crc >> 8) & 0x00ffffff);
+  }
+  write(buf2, offs + size - 4, byte4(crc ^ -1));
 }
 
 module.exports = PNGlib;
