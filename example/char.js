7b73dca2b49bf83022f22bc22a2c969c63b66d4e
geemo <153330685@qq.com>
2017-07-26 03:30:10 +0800

    perf: upgrade drawChar method speed to 500% ~ 600%

--- example/char.js
+++ example/char.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const path = require('path');
 const PNGlib = require('..');
 
-let png = new PNGlib(200, 200);
+let png = new PNGlib(200, 100);
 
 for (let i = 0; i < 10; ++i) {
   const font = PNGlib.font8x16;
   let ch = String(i);
   console.log(ch);
-  png.drawChar(ch, 0 + 2 * i * font.w, 100, font, '#00FF00');
+  png.drawChar(ch, 0 + 2 * i * font.w, 50, font, '#00FF00');
 }
 
 fs.writeFileSync(path.resolve(__dirname, './char.png'), png.getBuffer());
--- src/pnglib.js
+++ src/pnglib.js
@@ -126,13 +126,22 @@ class PNGlib {
       let l = Math.ceil(w / 8);
 
       for (let i = 0, len = font.h; i < len; ++i) {
-        let prevByteStr = utils.hexToBin(fontData[l * i]);
-        let nextByteStr = utils.hexToBin(fontData[l * i + 1]);
-        let line = (prevByteStr + nextByteStr).substr(0, w);
+        for (let j = 0; j < l; ++j) {
+          // 每一排的第几个字节
+          let d = fontData[l * i + j];
+          let width = 8;
+          let x0 = j * 8;          
+          // 假如到了这一排的最后一个字节
+          if (j === (l - 1)) {
+            width = w - x0;
+          }
 
-        for (let ci = 0, lineLen = line.length; ci < lineLen; ++ci) {
-          if (line[ci] === '1') {
-            this.setPixel(x + ci, y0, color); 
+          let mask = 1;
+          for (let bitPos = width - 1; bitPos >= 0; --bitPos) {
+            mask = 1 << bitPos;
+            if ((d & mask) === mask) {
+              this.setPixel(x + x0 + (8 - bitPos), y0, color);
+            }
           }
         }
         ++y0;
