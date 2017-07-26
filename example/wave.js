a5216b76a0489c308e9f9fe1065a8dbd7e242e49
Lellansin <lellansin@gmail.com>
2017-07-26 15:27:05 +0800

    doc: resize example (aim to captcha)

--- example/wave.js
+++ example/wave.js
@@ -4,17 +4,18 @@ const fs = require('fs');
 const path = require('path');
 const PNGlib = require('..');
 
-let png = new PNGlib(200, 150);
+let png = new PNGlib(100, 40);
 
-for (let i = 0, num = 200 / 10; i <= num; i += .01) {
+for (let i = 0, num = 100 / 10; i <= num; i += .01) {
 
   let x = i * 10;
   // Math.sin(i) range [-1, 1] ====> 0 <= y <= 50
-  let y = Math.sin(i) * 25 + 25;
+  let y = Math.sin(i) * 10 + 20;
 
   // use a color triad of Microsofts million dollar color
-  png.setPixel(x, (y), '#FF00FF');
-  png.setPixel(x, (y + 10), 'rgb(255,0,0)');
+  png.setPixel(x, (y - 10), '#0000FF');
+  png.setPixel(x, (y), '#FF0000');
+  png.setPixel(x, (y + 10), 'rgb(0,255,0)');
 }
 
 fs.writeFileSync(path.resolve(__dirname, './wave.png'), png.getBuffer());
