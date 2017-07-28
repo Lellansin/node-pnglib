e1560206532b8a54fee5b0a27c36a0e48a0982b7
Lellansin <lellansin@gmail.com>
2017-07-28 13:42:24 +0800

    fix: test arg

--- test/test.js
+++ test/test.js
@@ -18,7 +18,7 @@ describe('PNGlib', () => {
   describe('.setPixel', () => {
     it('should create a red 1 pix PNG.', () => {
       let png = new PNGlib(1, 1);
-      png.buffer[png.index(0, 0)] = png.color(255, 0, 0, 255);
+      png.buffer[png.index(0, 0)] = png.color([255, 0, 0, 255]);
       should.equal(png.getBase64(),
         'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAMAAAAoyzS7AAAAGFBMVEUAAAD/AAAAAAAAAAAAAAAAAAAAAAAAAAAAXHRiAAAACHRSTlMA/wAAAAAAACXRGJEAAAANSURBVHjaAQIA/f8AAQADAAL2gI4NAAAAAElFTkSuQmCC');
     });
